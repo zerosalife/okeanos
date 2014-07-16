474649b4bcfc2d5de132a680b228c7fe5ccb32a8
zerosalife <zerosalife@gmail.com>
2014-07-16 10:07:45 -0500

    commit begginings of project

---
+++ Assets/Scripts/BlueFishController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlueFishController : MonoBehaviour {
+
+  private Vector3 currentPosition;
+  public float moveSpeed;
+  private Vector3 currentDirection;
+  private bool changeDirection;
+
+  public int experiencePoints = 1;
+
+  public float knockbackAmount;
+  private Vector3 knockback;
+
+  // Use this for initialization
+  void Start () {
+    currentPosition = transform.position;
+
+    // Always change direction on the first frame.
+    currentDirection = Vector3.zero;
+    changeDirection = true;
+
+    // Calculate the direction for knockback.
+    knockback = knockbackAmount * Vector3.down;
+  }
+
+  // Update is called once per frame
+  void Update () {
+    currentPosition = transform.position;
+    if(changeDirection == true) {
+      currentDirection = RandomMoveDirection();
+      changeDirection = false;
+    }
+
+    Vector3 target = currentDirection * moveSpeed + currentPosition;
+    transform.position = Vector3.Lerp(currentPosition, target, Time.deltaTime);
+
+    if(changeDirection == false) {
+      if (Random.Range(0, 100) > 80) {
+        changeDirection = true;
+      }
+    }
+
+  }
+
+  void OnTriggerEnter2D(Collider2D collision) {
+    if(collision.gameObject.tag == "Player") {
+      GameObject playerObject = GameObject.FindGameObjectsWithTag("Player")[0];
+      playerObject.SendMessage("AddExperience", experiencePoints);
+      playerObject.SendMessage("Knockback", knockback);
+
+      Destroy(this.gameObject);
+    }
+  }
+
+  Vector3 RandomMoveDirection() {
+    Vector3 direction = Vector3.zero;
+    switch(Flip()) {
+    case 0:
+      direction = Vector3.right;
+      break;
+    case 1:
+      direction = Vector3.left;
+      break;
+    }
+    return direction;
+  }
+
+  int Flip() {
+    // Simulate a coin flip to produce 50:50 chance of [0, 1]
+    return Random.Range(0, 2);
+  }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+  private bool canSpawn;
+  private Vector3 currentPosition;
+  private int playerLevel;
+  public float moveSpeed = 1.5f;
+
+  // Use this for initialization
+  void Start () {
+    currentPosition = gameObject.transform.position;
+
+    // playerLevel = 1;
+  }
+
+  // Update is called once per frame
+  void Update () {
+    currentPosition = transform.position;
+
+    // Always march up.
+    Vector3 target = Vector3.up * moveSpeed + currentPosition;
+    transform.position = Vector3.Lerp(currentPosition, target, Time.deltaTime);
+
+
+  }
+
+  void AddExperience(int points) {
+    Debug.Log(points);
+  }
+
+  void Knockback(Vector3 distance) {
+    transform.position = currentPosition + distance;
+    currentPosition = transform.position;
+  }
+}
