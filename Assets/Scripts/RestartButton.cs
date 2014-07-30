24ce2ca22a843920d787f2ce9500cc2956c2665f
zerosalife <zerosalife@gmail.com>
2014-07-29 21:33:27 -0500

    accidentally forgot to add this to the previous commit 58cc441

---
+++ Assets/Scripts/RestartButton.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class RestartButton: MonoBehaviour {
+  void OnGUI() {
+    GUI.Label(new Rect(Screen.width / 2 - 30, 80, 100, 30), "Game Over");
+    if(GUI.Button(new Rect(Screen.width / 2 - 50,
+                           Screen.height / 2, 100, 50), "Restart")) {
+      Application.LoadLevel("Level01");
+    }
+
+  }
+}
