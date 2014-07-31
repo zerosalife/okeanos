d536a3ef40f08dd1fa4cf2a277e95f49d565770d
zerosalife <zerosalife@gmail.com>
2014-07-31 18:11:59 -0500

    added save and load functions to the GameController

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using System.Collections;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class GameController: MonoBehaviour {
   public static GameController control;
@@ -15,5 +18,38 @@ public class GameController: MonoBehaviour {
       // There can be only one!
       Destroy(gameObject);
     }
+  }
+
+  public void Save() {
+    BinaryFormatter bf = new BinaryFormatter();
+    FileStream file = File.Open(Application.persistentDataPath + "/gameInfo.dat",
+                     FileMode.Open);
+
+    PlayerData data = new PlayerData();
+    data.experiencePoints = experiencePoints;
+    data.playerLevel = playerLevel;
+
+    bf.Serialize(file, data);
+    file.Close();
+  }
+
+  public void Load() {
+    if(File.Exists(Application.persistentDataPath + "/gameInfo.dat")) {
+      BinaryFormatter bf = new BinaryFormatter();
+      FileStream file = File.Open(Application.persistentDataPath + "/gameInfo.dat",
+                                  FileMode.Open);
+      PlayerData data = (PlayerData)bf.Deserialize(file);
+      file.Close();
+
+      experiencePoints = data.experiencePoints;
+      playerLevel = data.playerLevel;
+    }
+  }
+
+  [Serializable]
+  class PlayerData {
+    // TODO: see about making gets and sets.
+    public int experiencePoints;
+    public int playerLevel;
   }
 }
